b9eb0679123a509cf49b9bf707c92257e25f3d79
Sebastian <sebas14121999@gmail.com>
2019-08-26 17:47:05 -0500

    Agregar archivos de proyecto.

---
+++ XamarinApp1/XamarinApp1/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new StackLayoutDemo();
+            //MainPage = new StackLayoutCode();
+            //MainPage = new AbsoluteLayoutDemo();
+            MainPage = new AbsoluteLayoutCode();
+            //MainPage = new RelativeLayoutDemo();
+            //MainPage = new RelativeLayoutCode();
+            //MainPage = new GridDemo();
+            //MainPage = new ScrollViewDemo();
+            //MainPage = new ScrollViewCode();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
